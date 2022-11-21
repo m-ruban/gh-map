57871c0ddb73530ee8447add9240fa27c43627bc
Ruban Mikhail <m.ruban@hh.ru>
2022-11-22 01:08:46 +0300

    add onResize

--- src/Map/modules/app.js
+++ src/Map/modules/app.js
@@ -1,6 +1,7 @@
 import { Application } from 'pixi.js';
 
 import { RESOLUTION } from 'map/modules/constants';
+import debounce from 'map/modules/debounce';
 
 // app
 const app = new Application({
@@ -19,5 +20,13 @@ map.appendChild(app.view);
 const canvas = map.querySelector('canvas');
 canvas.style.width = `${window.innerWidth}px`;
 canvas.style.height = `${window.innerHeight}px`;
+
+// resize
+const onResize = debounce(() => {
+    app.renderer.resize(window.innerWidth * RESOLUTION, window.innerHeight * RESOLUTION);
+    canvas.style.width = `${window.innerWidth}px`;
+    canvas.style.height = `${window.innerHeight}px`;
+}, 100);
+window.addEventListener('resize', onResize);
 
 export default app;
--- src/Map/modules/scrollable.ts
+++ src/Map/modules/scrollable.ts
@@ -2,11 +2,15 @@ import { FederatedPointerEvent } from 'pixi.js';
 
 import app from 'map/modules/app';
 import { COUNT_YEARS, WIDTH_YEAR } from 'map/modules/constants';
+import debounce from 'map/modules/debounce';
 
 app.stage.interactive = true;
 
+// borders
 const LEFT_SCROLL_BORDER: number = 0;
-const RIGHT_SCROLL_BORDER: number = (WIDTH_YEAR * COUNT_YEARS - app.view.width) * -1;
+let RIGHT_SCROLL_BORDER: number = (WIDTH_YEAR * COUNT_YEARS - app.view.width) * -1;
+
+// callbacks
 let prev: number | undefined;
 const onMove = (event: FederatedPointerEvent) => {
     if (prev) {
@@ -21,14 +25,20 @@ const onMove = (event: FederatedPointerEvent) => {
     }
     prev = event.global.x;
 };
-
 const onUpOrLeave: () => void = () => {
     prev = undefined;
     app.stage.off('pointermove', onMove);
 };
 
+// listeners
 app.stage.on('pointerdown', () => {
     app.stage.on('pointermove', onMove);
 });
 app.stage.on('pointerup', onUpOrLeave);
 app.stage.on('pointerleave', onUpOrLeave);
+
+// recalc border
+const onResize = debounce(() => {
+    RIGHT_SCROLL_BORDER = (WIDTH_YEAR * COUNT_YEARS - app.view.width) * -1;
+}, 200);
+window.addEventListener('resize', onResize);
