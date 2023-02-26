0635dcdc8f4e29b11815e57700eb121b6eb5ed00
Ruban Mikhail <m.ruban@hh.ru>
2023-02-26 19:54:42 +0300

    update mobile detect condiition

--- src/mobile.js
+++ src/mobile.js
@@ -1,3 +1,6 @@
-const mobile = () => navigator.userAgentData.mobile;
+const mobile = () => {
+    const agents = [/Android/i, /webOS/i, /iPhone/i, /iPad/i, /iPod/i, /BlackBerry/i, /Windows Phone/i];
+    return agents.some((toMatchItem) => navigator.userAgent.match(toMatchItem));
+};
 
 export default mobile;
