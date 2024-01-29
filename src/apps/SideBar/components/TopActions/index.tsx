87b047885b71b4acf9b19225da69cdc53561fade
Ruban Mikhail <m.ruban@hh.ru>
2024-01-29 23:48:05 +0600

    delete search temprorary

--- src/apps/SideBar/components/TopActions/index.tsx
+++ src/apps/SideBar/components/TopActions/index.tsx
@@ -3,7 +3,6 @@ import React, { FC } from 'react';
 import Help from 'side-bar/components/Help';
 import Info from 'side-bar/components/Info';
 import Logo from 'side-bar/components/Logo';
-import Search from 'side-bar/components/Search';
 
 import './top-actions.less';
 
@@ -17,7 +16,6 @@ const TopActions: FC = () => (
     <div className="top-actions">
         <Logo />
         <Help />
-        <Search />
         <Info />
     </div>
 );
