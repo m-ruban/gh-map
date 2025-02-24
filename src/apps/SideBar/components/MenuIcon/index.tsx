745211c0ddb93426a5e41c560e307aeda16246b1
Ruban Mikhail <m.ruban@hh.ru>
2025-02-24 23:54:12 +0100

    delete sidebar directory

--- src/apps/SideBar/components/MenuIcon/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react';
-
-import './menu-icon.less';
-
-const MenyIcon: FC<{ name: string; src: string; alt: string }> = ({ name, src, alt, ...props }) => {
-    return <img className={`menu-icon menu-icon_${name}`} src={src} alt={alt} {...props} />;
-};
-
-export default MenyIcon;
