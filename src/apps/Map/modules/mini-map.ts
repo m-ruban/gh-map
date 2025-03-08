7cfe9254c472700cd9d9cc9e5b9384075ba183de
Ruban Mikhail <m.ruban@hh.ru>
2025-03-08 19:39:48 +0100

    mini map refactoring

--- src/apps/Map/modules/mini-map.ts
+++ src/apps/Map/modules/mini-map.ts
@@ -1,6 +1,10 @@
-import { Application, Container } from 'pixi.js';
+import { Application, Container, FederatedPointerEvent, Rectangle } from 'pixi.js';
 
+import { dispatchCustomEvent } from 'src/modules/events';
+import MapEvent from 'src/modules/MapEvent';
 import mobile from 'src/modules/mobile';
+
+import { isCanvasTarget } from 'map/modules/listeners';
 
 let miniMap: Application<HTMLCanvasElement>;
 const root = document.querySelector('#mini-map') as HTMLElement;
@@ -17,15 +21,23 @@ if (!mobile()) {
         useContextAlpha: true,
         antialias: false,
     });
+    miniMap.stage.hitArea = new Rectangle(0, 0, miniMap.renderer.width, miniMap.renderer.height);
     root.appendChild(miniMap.view);
 }
 
-export const enableMiniMap = (miniMapContainer: Container): void => {
-    miniMap.stage.interactive = true;
+export const enableMiniMapAndAddListeners = (miniMapContainer: Container): void => {
     miniMap.view.style.visibility = 'visible';
     // stretch the container to the entire canvas
     miniMapContainer.width = originSize.width;
     miniMapContainer.height = originSize.height;
+
+    miniMap.stage.interactive = true;
+    miniMap.stage.on('pointerdown', (event: FederatedPointerEvent) => {
+        if (!isCanvasTarget(event)) {
+            return;
+        }
+        dispatchCustomEvent(MapEvent.ClickOnMimiMap, { detail: { event } });
+    });
 };
 
 export default miniMap;
